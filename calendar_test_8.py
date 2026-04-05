7b5f12f5516ce7a30fb69c10394d4f14c8edd746
hevarmette <splishsplash@vivalid.net>
2026-04-05 17:54:32 -0500

    notes

--- calendar_test_8.py
+++ calendar_test_8.py
@@ -1,6 +1,7 @@
 # remove stopped time in auto laps
 # TODO: functionality to save intensity values after editing and when selecting pill values
-# subtract paused time from auto laps
+# cumulative time on auto laps
+# fix x axis of time based graphs
 
 # add functionality to see average time by set and stats like that. and summary stats on the homepage or an extra page
 import streamlit as st
